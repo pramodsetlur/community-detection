0bb944c3ae6ec20ca8b86ed1415e604936181012
Pramod Setlur <pramod.setlur@ticketmaster.com>
2015-11-28 16:47:32 -0800

    Removing unwanted functions

--- pramod_setlur_communities.py
+++ pramod_setlur_communities.py
@@ -76,57 +76,6 @@ def compute_best_community(original_g):
 
     return communities, max_modularity
 
-def draw_graph(image, communities, graph):
-    G=nx.cubical_graph()
-    pos=nx.spring_layout(G) # positions for all nodes
-
-    # nodes
-    nx.draw_networkx_nodes(G,pos,
-                           nodelist=[0,1,2,3,4],
-                           node_color='r',
-                           node_size=500,
-                       alpha=0.8)
-    # nx.draw_networkx_nodes(G,pos,
-    #                        nodelist=[4,5,6,7],
-    #                        node_color='b',
-    #                        node_size=500,
-    #                    alpha=0.8)
-
-    # edges
-    nx.draw_networkx_edges(G,pos,width=1.0,alpha=0.5)
-    nx.draw_networkx_edges(G,pos,
-                           edgelist=[(0,1),(1,2),(2,3),(3,0)],
-                           width=8,alpha=0.5,edge_color='r')
-    # nx.draw_networkx_edges(G,pos,
-    #                        edgelist=[(4,5),(5,6),(6,7),(7,4)],
-    #                        width=8,alpha=0.5,edge_color='b')
-
-
-    # # some math labels
-    # labels={}
-    # labels[0]=r'$a$'
-    # labels[1]=r'$b$'
-    # labels[2]=r'$c$'
-    # labels[3]=r'$d$'
-    # labels[4]=r'$\alpha$'
-    # labels[5]=r'$\beta$'
-    # labels[6]=r'$\gamma$'
-    # labels[7]=r'$\delta$'
-    # nx.draw_networkx_labels(G,pos,labels,font_size=16)
-
-    plt.axis('off')
-    plt.savefig("labels_and_colors.png") # save as png
-    plt.show() # display
-
-
-def draw_graph2(graph):
-    #nx.draw_networkx(graph)
-    pos = nx.spring_layout(graph)
-    nx.draw_networkx(graph, pos)
-    plt.axis('off')
-    plt.savefig("labels_and_colors.png") # save as png
-    plt.show() # display
-
 def draw_graph(graph, communities, image):
     rand_value = random.random()
     val_map={}
